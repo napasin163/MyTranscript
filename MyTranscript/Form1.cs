75305ba4ab977fe91abd315bf323e6ca587d1749
63030041@kmitl.ac.th <63030041@kmitl.ac.th>
2021-11-04 23:31:22 +0700

    Add Data Grid

--- MyTranscript/Form1.cs
+++ MyTranscript/Form1.cs
@@ -12,9 +12,11 @@ namespace MyTranscript
 {
     public partial class Form1 : Form
     {
+        List<Course> year1Sem1;
         public Form1()
         {
             InitializeComponent();
+            year1Sem1 = new List<Course>();
         }
 
         class Course
@@ -32,6 +34,9 @@ namespace MyTranscript
             newCourse.CourseName = textBoxCourseName.Text;
             newCourse.CourseCredit = textBoxCourseID.Text;
             newCourse.Grade = textBoxCourseGrade.Text;
+
+            year1Sem1.Add(newCourse);
+            dataGridViewCourse.DataSource = year1Sem1;
         }
     }
 }
